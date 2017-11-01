891264d4c030ded8b09ae3bef4c82958b0e579b5
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-10-31 19:25:09 -0500

    Initial Commit

---
+++ start2.sh
@@ -0,0 +1,8 @@
+./rancher-compose --project-name sa-api-back \
+    --url http://192.168.99.100:8080/v1/projects/1a5 \
+    --access-key 970782B50D265F61035A \
+    --secret-key gdjSAuMHcrRms8oT6tRYy4UyMqn46ZNzXnCY7yKD \
+    -f docker-compose.yml \
+    --verbose up \
+    -d --force-upgrade \
+    --confirm-upgrade
