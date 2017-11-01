585411140bdaab5e122255e78e04fa4d6d9b7466
LAPTOP-B91FCI08\Noctis <roherrerap@unal.edu.co>
2017-10-31 19:49:49 -0500

    Update - File rancher-compose.yml

--- start2.sh
+++ start2.sh
@@ -1,4 +1,4 @@
-./rancher-compose --project-name sa-api-back \
+./rancher-compose --project-name contapp \
     --url http://192.168.99.100:8080/v1/projects/1a5 \
     --access-key 970782B50D265F61035A \
     --secret-key gdjSAuMHcrRms8oT6tRYy4UyMqn46ZNzXnCY7yKD \
