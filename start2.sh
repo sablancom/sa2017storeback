5f266339c7b0852b12691e0452fa8a1917830de4
sablancom <sablancom@unal.edu.co>
2017-11-02 08:40:31 -0500

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -1,7 +1,7 @@
 ./rancher-compose --project-name contapp \
     --url http://192.168.99.100:8080/v1/projects/1a5 \
-    --access-key 970782B50D265F61035A \
-    --secret-key gdjSAuMHcrRms8oT6tRYy4UyMqn46ZNzXnCY7yKD \
+    --access-key 9CDD083F3982E58A5ABB \
+    --secret-key eMeejjd9AVQiNPQAd3XCnXYgko41wMaCak2j7SFW \
     -f docker-compose.yml \
     --verbose up \
     -d --force-upgrade \
